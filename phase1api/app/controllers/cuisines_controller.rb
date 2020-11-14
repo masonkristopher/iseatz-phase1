436b70cfbaa7d1a24e1638873925595317aa4e08
masonkristopher <masonk000@gmail.com>
2020-11-14 10:53:42 -0600

    add cuisines, return complete response for endpoint 1

--- phase1api/app/controllers/cuisines_controller.rb
+++ phase1api/app/controllers/cuisines_controller.rb
@@ -5,6 +5,8 @@ class CuisinesController < ApplicationController
   def index
     api_key = request.headers['api-key']
     @response = Zomato::Cuisines.by_city(params['city'], api_key) if params['city']
+
+    # send response hash in json
     render :json => @response
   end
 end
--- phase1api/app/services/zomato.rb
+++ phase1api/app/services/zomato.rb
@@ -8,20 +8,46 @@ module Zomato
   # Make GET requests to Zomato API - get cuisines by city
   class Cuisines
     def self.by_city(city, key)
+      response = Hash.new
 
+      # get locations from Zomato
       conn = Faraday.new(url: 'https://developers.zomato.com/api/v2.1/cities') do |faraday|
         faraday.adapter Faraday.default_adapter
         faraday.response :json
       end
 
-      response = conn.get do |req|
+      cities_response = conn.get do |req|
         req.params['q'] = city
         req.headers['Content-Type'] = 'application/json'
         req.headers['user-key'] = key
       end
 
-      # puts response.body
-      response.body
+      # save first city from Zomato response
+      city = cities_response.body['location_suggestions'][0]
+
+      # set city_info in response hash
+      response['city_info'] = city
+
+      # store city id for cuisines request
+      city_id = city['id']
+
+      # get cuisines by city from Zomato
+      conn = Faraday.new(url: 'https://developers.zomato.com/api/v2.1/cuisines') do |faraday|
+        faraday.adapter Faraday.default_adapter
+        faraday.response :json
+      end
+
+      cuisines_response = conn.get do |req|
+        req.params['city_id'] = city_id
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['user-key'] = key
+      end
+
+      # add cuisines to response hash
+      response['cuisines'] = cuisines_response.body['cuisines']
+
+      # return response hash
+      response
     end
   end
 end
