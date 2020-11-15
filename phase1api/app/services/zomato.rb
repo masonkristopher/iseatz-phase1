4bbf369f7c1d69aa19388224eee34706c1785de3
masonkristopher <masonk000@gmail.com>
2020-11-15 11:05:39 -0600

    simplify request code, add cuisines console log

--- phase1api/app/services/zomato.rb
+++ phase1api/app/services/zomato.rb
@@ -8,38 +8,34 @@ module Zomato
   # Make GET requests to Zomato API - get cuisines by city
   class Cuisines
     def self.by_city(city, key)
+
+      puts 'Finding list of cuisines for ' + city
+
+      # create new hash to store city info and list of cuisines
       response = Hash.new
 
       # get locations from Zomato
       conn = Faraday.new(url: 'https://developers.zomato.com/api/v2.1/cities') do |faraday|
-        faraday.adapter Faraday.default_adapter
         faraday.response :json
       end
 
       cities_response = conn.get do |req|
         req.params['q'] = city
-        req.headers['Content-Type'] = 'application/json'
         req.headers['user-key'] = key
       end
 
-      # save first city from Zomato response
+      # parse city info
       city = cities_response.body['location_suggestions'][0]
-
-      # set city_info in response hash
       response['city_info'] = city
-
-      # store city id for cuisines request
       city_id = city['id']
 
       # get cuisines by city from Zomato
       conn = Faraday.new(url: 'https://developers.zomato.com/api/v2.1/cuisines') do |faraday|
-        faraday.adapter Faraday.default_adapter
         faraday.response :json
       end
 
       cuisines_response = conn.get do |req|
         req.params['city_id'] = city_id
-        req.headers['Content-Type'] = 'application/json'
         req.headers['user-key'] = key
       end
 
