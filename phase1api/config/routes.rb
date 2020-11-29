2d1c5c4e70dc8b30f148757855ccaab3b60a2928
masonkristopher <masonk000@gmail.com>
2020-11-29 11:07:35 -0600

    add rswag and generate initial files

--- phase1api/config/routes.rb
+++ phase1api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     resources :cuisines, only: :index
     resources :reviews, only: :index
