b576a2c6a98786d109ee9f3418289b3c680d0662
masonkristopher <masonk000@gmail.com>
2020-11-29 11:06:02 -0600

    refine cuisine response and check for 404 in cities req

--- phase1api/app/services/cuisine.rb
+++ phase1api/app/services/cuisine.rb
@@ -4,20 +4,36 @@ class Cuisine
     # make requests to Zomato API
     zomato_client = Zomato::Client.new(key)
     cities_response = zomato_client.cities(city)
+
+    # check for no results from cities
+    if cities_response['location_suggestions'].length < 1
+      return 404
+    end
+
     city = cities_response['location_suggestions'][0]
     cuisines_response = zomato_client.cuisines(city['id'])
 
     # parse response data into response hash
-    response = {}
-    response['city_info'] = {
+    response = {
       :id => city['id'],
       :name => city['name'],
       :state => city['state_name'],
       :state_code => city['state_code'],
       :country => city['country_name'],
     }
-    response['cuisines'] = cuisines_response['cuisines']
-      .map{|item| item['cuisine']['cuisine_name']}
+
+    # add cuisines to response hash
+    if cuisines_response['cuisines'].length < 1
+      response['cuisines'] = []
+    else
+      response['total_cuisines'] = cuisines_response['cuisines'].length
+      response['cuisines'] = cuisines_response['cuisines']
+        .map{|item| {
+          :name => item['cuisine']['cuisine_name'],
+          :id => item['cuisine']['cuisine_id']
+          }}
+    end
+
     response
   end
 end
