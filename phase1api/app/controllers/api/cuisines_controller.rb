693b60ccfd6f77468682cb73edd2c9ac85912e64
masonkristopher <masonk000@gmail.com>
2020-11-29 11:04:52 -0600

    add basic param checking and 404 error handling

--- phase1api/app/controllers/api/cuisines_controller.rb
+++ phase1api/app/controllers/api/cuisines_controller.rb
@@ -6,16 +6,19 @@ class Api::CuisinesController < ApplicationController
     api_key = request.headers['api-key']
 
     if !api_key && !params['city']
-      return render :json => 'Missing city and API key', :status => 400
+      return render :json => 'Missing city query and API key', :status => 400
     elsif !api_key
       return render :json => 'Missing API key', :status => 400
     elsif !params['city']
-      return render :json => 'Missing city', :status => 400
+      return render :json => 'Missing city query', :status => 400
     end
 
-    @response = Cuisine.by_city(params['city'], api_key)
-    render :json => @response
+    response = Cuisine.by_city(params['city'], api_key)
 
+    if response == 404
+      return render :json => "No match found for #{params['city']} in cities", :status => 404
+    end
+    render :json => response
 
   end
 end
--- phase1api/app/controllers/api/reviews_controller.rb
+++ phase1api/app/controllers/api/reviews_controller.rb
@@ -3,16 +3,22 @@ class Api::ReviewsController < ApplicationController
     api_key = request.headers['api-key']
 
     if !api_key && !params['city_id'] && params['cuisine_id']
-      return render :json => 'Missing city ID, cuisine ID, and API key', :status => 400
+      return render :json => 'Missing city and cuisines IDs, and API key', :status => 400
     elsif !api_key
       return render :json => 'Missing API key', :status => 400
+    elsif !params['city_id'] && !params['cuisine_id']
+      return render :json => 'Missing city and cuisine ID', :status => 400
     elsif !params['city_id']
       return render :json => 'Missing city ID', :status => 400
     elsif !params['cuisine_id']
       return render :json => 'Missing cuisine ID', :status => 400
     end
 
-    @response = Review.by_city_and_cuisine(params['city_id'], params['cuisine_id'], api_key)
-    render :json => @response
+    response = Review.by_city_and_cuisine(params['city_id'], params['cuisine_id'], api_key)
+
+    if response == 404
+      return render :json => "No match found for city and cuisine search", :status => 404
+    end
+    render :json => response
   end
 end
